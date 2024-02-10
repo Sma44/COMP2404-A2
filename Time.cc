ad089669bab0786f46c23f7e386b53ae65ffe819
Sma44 <samsonma44@gmail.com>
2024-02-09 21:05:55 -0500

    modified Time class

--- Time.cc
+++ Time.cc
@@ -28,3 +28,67 @@ void Time::print()
        << right << setfill('0')<<setw(2)<<minutes;
 }
 
+bool Time::computeSlot(TimeSlotType& slot){
+
+  switch(hours){
+    case 8:
+      slot=SLOT_0830;
+      return true;
+    case 10:
+      slot=SLOT_1000;
+      return true;
+    case 11:
+      slot=SLOT_1130;
+      return true;
+    case 13:
+      slot=SLOT_1300;
+      return true;
+    case 14:
+      slot=SLOT_1430;
+      return true;
+    case 16:
+      SLOT_1600;
+      return true;
+    case 18:
+      slot=SLOT_1800;
+      return true;
+    default:
+      slot=BAD_TIME_SLOT;
+      cout << endl << "error in the time" << endl;
+      break;
+  }
+
+  return false;
+}
+
+bool Time::setFromSlot(TimeSlotType slot){
+
+    switch(slot){
+    case SLOT_0830:
+      set(8,30);
+      return true;
+    case SLOT_1000:
+      set(10,0);
+      return true;
+    case SLOT_1130:
+      set(11,30);
+      return true;
+    case SLOT_1300:
+      set(13,0);
+      return true;
+    case SLOT_1430:
+      set(13,0);
+      return true;
+    case SLOT_1600:
+      set(13,0);
+      return true;
+    case SLOT_1800:
+      set(13,0);
+      return true;
+    case BAD_TIME_SLOT:
+      set(13,0);
+      return false;
+  }
+
+  return false;
+}
--- Time.h
+++ Time.h
@@ -1,10 +1,14 @@
 #ifndef TIME_H
 #define TIME_H
 
+#include "defs.h"
+
 class Time
 {
   public:
     Time(int=0, int=0);
+    bool computeSlot(TimeSlotType&);
+    bool setFromSlot(TimeSlotType);
     void print();
 
   private:
