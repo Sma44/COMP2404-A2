a4e05cb5ada21e1ad7c8b76a15cc8de433453ea6
Sma44 <samsonma44@gmail.com>
2024-02-13 01:04:29 -0500

    print function implemented(?)

--- Schedule.cc
+++ Schedule.cc
@@ -45,23 +45,71 @@ bool Schedule::addCourse(Course* course){
 }
 
 // TODO: WTF is this shit, also use iomanip apparently
-// maybe make helper to print line break
-// the time box is a 9ch x 3ln unit
-// horizontal line is 104ch long
-// 5ch space for labels, then 10ch for data
-// blocks are 15ch wide
-// 3 ch gap between end of one block and start of next
-// perhaps 1ch padding on both sides, then 1 ch space between blocks
-// 4ch space into a block before printing date
-// 3ch space between start of first block and the time box
-// maybe use a case statement for how the line printed is formatted
 void Schedule::print(){
+
+    // print banner 
     cout << endl << "   Schedule for current term: " << term << endl << endl;
+    
+    // prints labels for days
     printHorizontalLine();
-    cout << "|" + string(12,' ');
+    cout << "|" << setfill(' '); // prints left side bar for label
+    for (int i = 0; i < NUM_DAYS; i++){ // prints each day with padding
+        cout << setw(18) << right <<((WeekDayType)i);
+    }
+    cout << setw(14) << right << "|" << endl; // prints right side padding for label
+    printHorizontalLine(); // closes label
 
 
+    // temp time to call print()s on
+    Time t(0,0);
 
+    for (int i = 0; i < NUM_SLOTS; i++){ // for each timeslot row
+
+        for (int j = 0; j < 3; j++){ // loop 3 times for each row
+            switch(j){ // print date on first loop, then print spaces instead
+                case 0: 
+                    t.setFromSlot((TimeSlotType)i);
+                    cout << setw(2) << left << "|";
+                    t.print();
+                    cout << setw(2) << right << "|";
+                    cout << setw(4) << " "; // add 4 space padding before printing courses
+                    break;
+                case 1: 
+                case 2:
+                    cout << setw(8) << left << "|";
+                    cout << setw(5) << left << "|"; // add 4 space padding
+                    break;
+            }
+
+            for (int k = 0; k < NUM_DAYS; k++){ // loop through every course in row
+                if (courses[k][i] != nullptr){ // if entry exists
+                    switch(j){ // print the corresponding data per iteration
+                        case 0: 
+                            cout << setw(18) << left << courses[k][i]->getCourse(); // label and data with padding
+                            break;
+                        case 1:
+                            cout << setw(5) << left << "CN:"; // label
+                            cout << setw(13) << left << courses[k][i]->getId(); // data with padding
+                            break;
+                        case 2:
+                            cout << setw(5) << left << "Pr:"; // label
+                            cout << setw(13) << left << courses[k][i]->getInstructor(); // data with padding
+                            break;
+                    }
+                }
+                else{ // if a nullptr, just add padding as placeholder
+                    cout << string(18,' ');
+                }
+
+
+            }
+            // padding at the end of the row
+            cout << setw(4) << right << "|" << endl;
+        }
+
+        // prints bottom horizontal line to enclose block
+        printHorizontalLine();
+    }
 }
 
 void Schedule::clear(){
@@ -76,7 +124,8 @@ string Schedule::getTerm(){
     return term;
 }
 
-// helper to print line, maybe remove if using iomanip
+// helper function
 void Schedule::printHorizontalLine(){
-    cout << string(104, '-') << endl;
+    cout << setw(1) << "|" << setw(103) << right << setfill('-');
+    cout << setw(1) << setfill(' ')<< "|" << endl;
 }
